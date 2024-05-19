30803a8fa58583d2410b1b92e107a9655d801738
NairHar <hk1987.nair@yahoo.com>
2024-05-19 12:01:52 +0100

    newly merged changes

--- price.py
+++ price.py
@@ -7,44 +7,51 @@ class Price:
     r = None
     sigma = None
     T = None
+    b = None
+    q = None
     
-    def __init__(self, S, K, r, sigma, T):
+    def __init__(self, S, K, r, sigma, T, q):
         self.S = S
         self.K = K
         self.r = r
         self.sigma = sigma
         self.T = T
+        self.b = r - q
         return    
     
     def d1(self):
-        return (np.log(self.S/self.K) + (self.r + ((self.sigma**2)/2) *\
+        res = (np.log(self.S/self.K) + ((self.b + ((self.sigma**2)/2)) *\
             self.T))/(self.sigma * np.sqrt(self.T))
+        return res
     
     def d2(self):
-        return (np.log(self.S/self.K) + (self.r - ((self.sigma**2)/2) *\
-            self.T))/(self.sigma * np.sqrt(self.T))
+        return self.d1()- (self.sigma * np.sqrt(self.T))
         
     def call(self):
         d1_ = self.d1()
         d2_ = self.d2()
         
-        res = self.S*norm.cdf(d1_) - (self.K * np.exp(-self.r * self.T) *\
+        res = (self.S* np.exp((self.b -self.r)*self.T) * norm.cdf(d1_)) - \
+            (self.K * np.exp(-self.r * self.T) *\
             norm.cdf(d2_))
-        return (format(res, ".5f"))
+        return (round(res, 5))
             
     def put(self):
         d1_ = self.d1()
         d2_ = self.d2()
         
-        res= self.S*norm.cdf(d1_) - (self.K * np.exp(-self.r * self.T) *\
-            norm.cdf(d2_))-(self.S-self.K*np.exp(-self.r*self.T))
-        return (format(res, ".5f"))
+        res= (self.K * np.exp(-self.r * self.T) * norm.cdf(-d2_)) - \
+            (self.S * np.exp((self.b - self.r) * self.T) * norm.cdf(-d1_))
     
+        return (round(res, 5))
+    
+    # first-order sensitivity
     def delta(self):
         res = self.d1()
-        return res 
+        return (format(res, ".5f")) 
     
+    # second-order sensitivity
     def gamma(self):
-        res = self.d() - (self.sigma * np.sqrt(self.T))
-        return res
+        res = self.d1() - (self.sigma * np.sqrt(self.T))
+        return (format(res, ".5f"))
     
--- test/test_vanillaoption.py
+++ test/test_vanillaoption.py
@@ -6,12 +6,12 @@ import price as bs
 class TestVanilla(unittest.TestCase):
     
     def test_call(self):
-        call_price = bs.Price(60,65,0.08,0.30,0.25)
+        call_price = bs.Price(60,65,0.08,0.30,0.25,0)
         result = call_price.call()
         self.assertEqual(result,2.13337)
         
     def test_put(self):
-        put_price = bs.Price(60,65,0.08,0.30,0.25)
+        put_price = bs.Price(60,65,0.08,0.30,0.25,0)
         result = put_price.put()
         self.assertEqual(result,5.84628)
         
