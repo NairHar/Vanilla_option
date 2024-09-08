cdafad51aea115e4f62eaf02b328633678ea75bd
NairHar <hk1987.nair@yahoo.com>
2024-09-08 07:51:08 +0100

    changed testcase

--- test/test_vanillaoption.py
+++ test/test_vanillaoption.py
@@ -1,7 +1,7 @@
 import pytest
 from bs import price as bs
 
-class TestVanilla(unittest.TestCase):
+class TestVanilla:
     
     def test_call(self):
         call_price = bs.Price(60,65,0.08,0.30,0.25,0)
