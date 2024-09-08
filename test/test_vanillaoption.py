4a633c18c34b85fd8251a0add694893cc0a560d7
NairHar <hk1987.nair@yahoo.com>
2024-09-08 07:58:36 +0100

    remov unitest

--- test/test_vanillaoption.py
+++ test/test_vanillaoption.py
@@ -1,17 +1,17 @@
-import unittest
+import pytest
 from bs import price as bs
 
-class TestVanilla(unittest.TestCase):
+class TestVanilla:
     
     def test_call(self):
         call_price = bs.Price(60,65,0.08,0.30,0.25,0)
         result = call_price.call()
-        self.assertEqual(result,2.13337)
+        assert result==2.13337
         
     def test_put(self):
         put_price = bs.Price(60,65,0.08,0.30,0.25,0)
         result = put_price.put()
-        self.assertEqual(result,5.84628)
+        assert result==5.84628
         
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
